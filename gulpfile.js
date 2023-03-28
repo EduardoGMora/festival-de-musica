36546ace16e913a786f527348349516bd139a5de
Eduardo Mora <99228341+EduardoGMora@users.noreply.github.com>
2023-03-27 20:02:39 -0600

    primera actualización

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
-function tarea(done) {
-    console.log('Mi primer tarea');
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber=require('gulp-plumber');                 //para que no se deje de ejecutar el workflow
 
-    done();
+//src es para buscar un archivo
+//dest nos permite almacenar info en una carpeta de estilo
+
+function css(done) {
+
+    src("src/scss/**/*.scss")                //identifica el archivo de sass
+        .pipe( plumber())
+        .pipe( sass() )                     //compilar sass
+        .pipe( dest("build/css") );   //almacenar en el ssd
+
+    done(); //callback
 }
 
-exports.tarea = tarea;
+function dev(done){
+    watch("src/scss/**/*.scss",css);
+
+    done()
+}
+
+exports.css=css;
+exports.dev=dev;
